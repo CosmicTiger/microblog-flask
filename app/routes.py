7f313de44e04a6e2a7cf4c12afa935fe7a08acd7
CosmicTiger <luisangelmarcia@gmail.com>
2020-05-26 00:45:45 -0600

    :construction: - Luisangel Marcia - v0.2 Adding templates dir and applications of Jinja over the project, also __pycache__ dirs untracked

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'Joker_LotusZero'}
+
+    posts = [
+        {
+            'author': {'username': 'Juan'},
+            'body': 'Beautiful Night in Nicaragua!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'CS50x.ni Is Cool'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
