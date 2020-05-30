5338031954aeff0e6f9b793535fc7bd914ef2705
CosmicTiger <luisangelmarcia@gmail.com>
2020-05-29 23:04:47 -0600

    :construction: - Luisangel Marcia - Adding shell context for flask shell, adding User Logins and Register module

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sh0w1ng-y0ur-tru3-f0rm'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFCATIONS = False
